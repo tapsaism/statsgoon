4c2bd5278b29b7e67b13aceec5bed8c4f309335c
Olli Iivonen <olliiivonen@Ollis-MacBook-Pro.local>
2017-02-19 15:34:59 +0200

    SQL statements split into to separate files

--- sql/statsgoon_create_views.sql
+++ sql/statsgoon_create_views.sql
@@ -1,154 +1,4 @@
 
-/** PHYSICAL TABLES **/
-
-/** CREATE SCHEDULE **/
-
-/*
-DROP TABLE nhlSchedule
-
-CREATE TABLE nhlSchedule
-(
-gameDayDate date,
-gameDesc varchar(100),
-homeTeam varchar(50),
-awayTeam varchar(50)
-)
-
-SELECT * FROM nhlSchedule
-
-COPY nhlSchedule FROM STDIN
-
- '/users/shared/HockeyGMDadaSchedule_NHL.tsv'
- */
- 
- /*
-DROP TABLE nhlScheduleGameDayIndex
-
-CREATE TABLE nhlScheduleGameDayIndex
-AS
-select
-gamedaydate,
-gamedesc,
-hometeam,
-awayteam,
-dense_rank() over(ORDER BY gamedaydate) + 3 as GameDayIndex,
-CASE 
-	WHEN gamedaydate between '2016-10-12' AND '2016-11-13' THEN 1
-	WHEN gamedaydate between '2016-11-17' AND '2016-12-11' THEN 2
-	WHEN gamedaydate between '2016-12-15' AND '2017-01-26' THEN 3
-	WHEN gamedaydate between '2017-01-31' AND '2017-03-05' THEN 4
-	WHEN gamedaydate between '2016-03-09' AND '2016-04-09' THEN 5
-END AS HockeyGMPeriod
-
-from nhlSchedule
-
-ORDER BY 1
-*/
-
-/** CREATE IMPORT TABLES **/
-
-/*
-
-DROP TABLE hockeygm_stats_f
-
-CREATE TABLE hockeygm_stats_f
-(
-	insertdate date,
-	filedate int,
-	team VARCHAR(50),
-	name VARCHAR(100),
-	games int,
-	goals int,
-	assists int,
-	points int,
-	shg_goals int,
-	shg_assists int,
-	gw_goals int,
-	ot_goals int,
-	shots int,
-	hits int,
-	blocks int,
-	fo_wins int,
-	fo_losses int,
-	plusminus int,
-	penaltymin int,
-	three_stars int,
-	two_stars int,
-	one_stars int,
-	hgm_avg float,
-	hgm_total int,
-	hgm_value VARCHAR(15)
-)
-
-SELECT DISTINCT name FROM hockeygm_stats_f 
-
-ORDER BY insertdate DESC
-
-
-DROP TABLE hockeygm_stats_d
-
-CREATE TABLE hockeygm_stats_d
-(
-	insertdate date,
-	filedate int,
-	team VARCHAR(50),
-	name VARCHAR(100),
-	games int,
-	goals int,
-	assists int,
-	points int,
-	shg_goals int,
-	shg_assists int,
-	gw_goals int,
-	ot_goals int,
-	shots int,
-	hits int,
-	blocks int,
-	fo_wins int,
-	fo_losses int,
-	plusminus int,
-	penaltymin int,
-	three_stars int,
-	two_stars int,
-	one_stars int,
-	hgm_avg float,
-	hgm_total int,
-	hgm_value VARCHAR(15)
-)
-
-SELECT * FROM hockeygm_stats_d ORDER BY INSERTDATE DESC
-
-/** **/
-
---DROP TABLE hockeygm_stats_g
-
-CREATE TABLE hockeygm_stats_g
-(
-	insertdate date,
-	filedate int,
-	team VARCHAR(50),
-	name VARCHAR(100),
-	games int,
-	goals int,
-	assists int,
-	points int,
-	wins int,
-	losses int,
-	ot_losses int,
-	saves int,
-	allowed_goals int,
-	shutouts int,
-	penalties int,
-	three_stars int,
-	two_stars int,
-	one_stars int,
-	hgm_avg float,
-	hgm_total int,
-	hgm_value VARCHAR(15)
-)
-
-SELECT * FROM hockeygm_stats_g ORDER BY INSERTDATE DESC
-
 */
 
 /** VIEWS **/
@@ -162,25 +12,37 @@ SELECT * FROM hockeygm_stats_g ORDER BY INSERTDATE DESC
 				-- 1.3.1.1.1 getPlayerData()
 	-- 1.4. V_PlayersAllGames
 
+
+/** DROP VIEWS **/
+
+--DROP FUNCTION getPlayerData();
+--DROP VIEW V_stats;
+--DROP VIEW V_PlayerStatsLastGames;
+--DROP VIEW V_PlayerStatsLatest;
+--DROP VIEW V_PlayerStatsDaily;
+--DROP VIEW v_gamesperperiod;
+--DROP VIEW V_PlayersAllGames;
+--DROP VIEW V_PlayerStatsAll;
+
 /** V_PlayerStatsAll **/
 
 /** DESC: Combine goalie, dmen and forward stats **/
 
 /** DEPENDENCIES: import tables **/
 
-DROP VIEW V_PlayerStatsAll
+--DROP VIEW V_PlayerStatsAll
 
-CREATE VIEW V_PlayerStatsAll
+CREATE OR REPLACE VIEW V_PlayerStatsAll
 AS
-SELECT 
-* 
+SELECT
+*
 FROM (
 
-SELECT 
+SELECT
 filedate,
 team,
 'FWD' AS position,
-CASE 
+CASE
 	WHEN split_part(name, ' ' , 3) = '' THEN split_part(name, ' ' , 2) || ' ' || split_part(name, ' ' , 1)
 	ELSE split_part(name, ' ' , 3) || ' ' || split_part(name, ' ' , 1) || ' ' || split_part(name, ' ' , 2)
 END AS player,
@@ -217,11 +79,11 @@ FROM hockeygm_stats_f
 
 UNION
 
-SELECT 
+SELECT
 filedate,
 team,
 'DEF' AS position,
-CASE 
+CASE
 	WHEN split_part(name, ' ' , 3) = '' THEN split_part(name, ' ' , 2) || ' ' || split_part(name, ' ' , 1)
 	ELSE split_part(name, ' ' , 3) || ' ' || split_part(name, ' ' , 1) || ' ' || split_part(name, ' ' , 2)
 END AS player,
@@ -240,16 +102,16 @@ goals * 9
 + shg_assists * 2
 + gw_goals * 2
 + ot_goals * 3) AS hgm_total_wo_ga,
-CASE 
+CASE
 WHEN hgm_total > 0 THEN
 	CAST((goals * 9
 	+ assists * 6
 	+ shg_goals * 4
 	+ shg_assists * 2
 	+ gw_goals * 2
-	+ ot_goals * 3 ) AS FLOAT) / hgm_total 
-ELSE 0 
-END 
+	+ ot_goals * 3 ) AS FLOAT) / hgm_total
+ELSE 0
+END
 AS hgm_ga_percentage_total,
 REPLACE(REPLACE(hgm_value,'€',''),' ','') as hgm_value
 
@@ -257,11 +119,11 @@ FROM hockeygm_stats_d
 
 UNION
 
-SELECT 
+SELECT
 filedate,
 team,
 'GOA' AS position,
-CASE 
+CASE
 	WHEN split_part(name, ' ' , 3) = '' THEN split_part(name, ' ' , 2) || ' ' || split_part(name, ' ' , 1)
 	ELSE split_part(name, ' ' , 3) || ' ' || split_part(name, ' ' , 1) || ' ' || split_part(name, ' ' , 2)
 END AS player,
@@ -273,15 +135,15 @@ goals * 25 + assists * 10 AS hgm_total_ga,
 hgm_total - (goals * 25 + assists * 10) AS hgm_total_wo_ga,
 CASE
 WHEN hgm_total > 0 THEN
-CAST((goals * 25 + assists * 10) AS FLOAT) / hgm_total 
+CAST((goals * 25 + assists * 10) AS FLOAT) / hgm_total
 ELSE 0
-END 
+END
 AS hgm_ga_percentage_total,
 REPLACE(REPLACE(hgm_value,'€',''),' ','') as hgm_value
 
 FROM hockeygm_stats_g
 
-) AS Skaters
+) AS Skaters;
 
 /** V_PlayerStatsLatest **/
 
@@ -289,20 +151,20 @@ FROM hockeygm_stats_g
 
 /** DEPENDENCIES: V_PlayerStatsAll **/
 
-DROP VIEW V_PlayerStatsLatest
+--DROP VIEW V_PlayerStatsLatest
 
-CREATE VIEW V_PlayerStatsLatest
+CREATE OR REPLACE VIEW V_PlayerStatsLatest
 
 AS
 
-SELECT 
-* 
-FROM V_PlayerStatsAll 
+SELECT
+*
+FROM V_PlayerStatsAll
 WHERE filedate = (
-					SELECT 
-						MAX(filedate) 
+					SELECT
+						MAX(filedate)
 					FROM V_PlayerStatsAll
-				)
+				);
 
 /** v_gamesPerPeriod **/
 
@@ -310,11 +172,11 @@ WHERE filedate = (
 
 /** DEPENDENCIES: nhlScheduleGameDayIndex **/
 
-DROP VIEW v_gamesPerPeriod
+--DROP VIEW v_gamesPerPeriod
 
-CREATE VIEW v_gamesPerPeriod
+CREATE OR REPLACE VIEW v_gamesPerPeriod
 AS
-SELECT 
+SELECT
 homeGames.hockeygmperiod,
 homeGames.team,
 homeGames.games + awayGames.games AS totalGames,
@@ -322,7 +184,7 @@ homeGames.games AS homeGames,
 awayGames.games AS awayGames
 FROM (
 
-SELECT 
+SELECT
 hockeygmperiod,
 hometeam AS team,
 count(*) AS games
@@ -336,19 +198,19 @@ GROUP BY hockeygmperiod, hometeam
 
 INNER JOIN (
 
-		SELECT 
+		SELECT
 		hockeygmperiod,
 		awayteam AS team,
 		count(*) AS games
 		FROM nhlScheduleGameDayIndex
-		
+
 		WHERE hockeygmperiod IS NOT NULL
-		
+
 		GROUP BY hockeygmperiod, awayteam
 
 ) AS awayGames
 ON homeGames.team = awayGames.team
-AND homeGames.hockeygmperiod = awayGames.hockeygmperiod
+AND homeGames.hockeygmperiod = awayGames.hockeygmperiod;
 
 /** V_PlayerStatsDaily **/
 
@@ -356,13 +218,13 @@ AND homeGames.hockeygmperiod = awayGames.hockeygmperiod
 
 /** DEPENDENCIES: V_PlayerStatsAll **/
 
-DROP VIEW V_PlayerStatsDaily
+--DROP VIEW V_PlayerStatsDaily
 
-CREATE VIEW V_PlayerStatsDaily
+CREATE OR REPLACE VIEW V_PlayerStatsDaily
 
 AS
 
-SELECT 
+SELECT
 filedate,
 team,
 position,
@@ -376,7 +238,7 @@ gamesplayed - LAG(gamesplayed,1) OVER(PARTITION BY player ORDER BY filedate) AS 
 
 FROM V_PlayerStatsAll
 
-ORDER BY filedate
+ORDER BY filedate;
 
 /** V_PlayerStatsLastGames **/
 
@@ -384,13 +246,13 @@ ORDER BY filedate
 
 /** DEPENDENCIES: V_PlayerStatsDaily **/
 
-DROP VIEW V_PlayerStatsLastGames
+--DROP VIEW V_PlayerStatsLastGames
 
-CREATE VIEW V_PlayerStatsLastGames
+CREATE OR REPLACE VIEW V_PlayerStatsLastGames
 
 AS
 
-SELECT 
+SELECT
 filedate,
 team,
 position,
@@ -410,7 +272,7 @@ median
 
 FROM (
 
-	SELECT 
+	SELECT
 	filedate,
 	team,
 	position,
@@ -434,19 +296,19 @@ FROM (
 	ROW_NUMBER() OVER (PARTITION BY player ORDER BY filedate DESC) AS latest,
 	STDDEV(Points) OVER (PARTITION BY player) AS std_dev,
 	MEDIAN(Points) OVER (PARTITION BY player) AS median
-	  
-	FROM V_PlayerStatsDaily 
-	
+
+	FROM V_PlayerStatsDaily
+
 	WHERE game_played = 1
 
 ) AS last_five_games
 
-WHERE latest = 1 
-AND last_10_period < 30
-AND last_5_period < 15
-AND last_3_period < 10
+WHERE latest = 1
+--AND last_10_period < 30
+--AND last_5_period < 15
+--AND last_3_period < 10
 
-ORDER BY median desc
+ORDER BY median desc;
 
 /** V_stats **/
 
@@ -454,13 +316,13 @@ ORDER BY median desc
 
 /** DEPENDENCIES: V_PlayerStatsLatest, V_PlayerStatsLastGames, getPlayerData() **/
 
-DROP VIEW V_stats
+--DROP VIEW V_stats
 
-CREATE VIEW V_stats 
+CREATE OR REPLACE VIEW V_stats
 
 AS
 
-SELECT 
+SELECT
 latestStats.filedate,
 latestStats.team,
 latestStats.position,
@@ -471,7 +333,7 @@ latestStats.position || ' - ' || latestStats.player || ' - ' || latestStats.hgm_
 latestStats.hgm_value as value,
 case when latestStats.position = 'FWD' then 1 when latestStats.position = 'DEF' then 0 when latestStats.position = 'GOA' then 0 end as forward,
 case when latestStats.position = 'FWD' then 0 when latestStats.position = 'DEF' then 1 when latestStats.position = 'GOA' then 0 end as defence,
-case when latestStats.position = 'FWD' then 0 when latestStats.position = 'DEF' then 0 when latestStats.position = 'GOA' then 1 end as goalie,                
+case when latestStats.position = 'FWD' then 0 when latestStats.position = 'DEF' then 0 when latestStats.position = 'GOA' then 1 end as goalie,
 latestStats.hgm_avg as points_avg,
 latestStats.hgm_total as points_total,
 latestStats.hgm_total_ga as points_ga_only,
@@ -494,7 +356,7 @@ FROM V_PlayerStatsLatest latestStats
 LEFT JOIN V_PlayerStatsLastGames lastGame
 ON latestStats.team = lastGame.team
 AND latestStats.player = lastGame.player
-AND latestStats.position = lastGame.position
+AND latestStats.position = lastGame.position;
 
 /** getPlayerData() **/
 
@@ -502,16 +364,16 @@ AND latestStats.position = lastGame.position
 
 /** DEPENDENCIES: V_PlayerStatsLatest, V_PlayerStatsLastGames, getPlayerData() **/
 
-DROP FUNCTION getPlayerData()
+--DROP FUNCTION getPlayerData()
 
-CREATE FUNCTION getPlayerData()
+CREATE OR REPLACE FUNCTION getPlayerData()
 RETURNS SETOF V_stats AS
 $DELIMETER$
-  
+
   SELECT * FROM V_stats
 
 $DELIMETER$
-LANGUAGE 'sql'
+LANGUAGE 'sql';
 
 /** V_PlayersAllGames **/
 
@@ -519,9 +381,9 @@ LANGUAGE 'sql'
 
 /** DEPENDENCIES: V_playerStatsAll, nhlScheduleGameDayIndex **/
 
-DROP VIEW V_PlayersAllGames
+--DROP VIEW V_PlayersAllGames
 
-CREATE VIEW V_PlayersAllGames
+CREATE OR REPLACE VIEW V_PlayersAllGames
 
 AS
 
@@ -529,10 +391,10 @@ SELECT
 *
 FROM (
 	--SELECT ALL PLAYERS
-	select 
-	distinct 
-	player, 
-	team 
+	select
+	distinct
+	player,
+	team
 	from V_playerStatsAll
 	where player is not null
 ) AS players
@@ -548,22 +410,67 @@ SELECT
 *
 FROM (
 	--SELECT ALL PLAYERS
-	select 
-	distinct 
-	player, 
-	team 
+	select
+	distinct
+	player,
+	team
 	from V_playerStatsAll
 	where player is not null
 ) AS players
 
 --JOIN AWAY GAMES TO PLAYERS
 LEFT JOIN nhlScheduleGameDayIndex awayGames
-ON awayGames.awayTeam = players.team
+ON awayGames.awayTeam = players.team;
+
+SELECT COUNT(*) AS row_count, 'getPlayerData' AS view_name FROM getPlayerData()
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'V_stats' AS view_name FROM V_stats
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'V_PlayerStatsLastGames' AS view_name FROM V_PlayerStatsLastGames
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'V_PlayerStatsLatest' AS view_name FROM V_PlayerStatsLatest
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'V_PlayerStatsDaily' AS view_name FROM V_PlayerStatsDaily
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'v_gamesperperiod' AS view_name FROM v_gamesperperiod
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'V_PlayersAllGames' AS view_name FROM V_PlayersAllGames
+
+UNION
+
+SELECT COUNT(*) AS row_count, 'V_PlayerStatsAll' AS view_name FROM V_PlayerStatsAll;
 
 
+-- 1. V_PlayerStatsAll
+	-- 1.2. V_PlayerStatsLatest
+		-- 1.2.1 V_stats
+	-- 1.3. V_PlayerStatsDaily
+		-- 1.3.1 v
+			-- 1.3.1.1 V_stats
+				-- 1.3.1.1.1 getPlayerData()
+	-- 1.4. V_PlayersAllGames
 
+/**
 
+DROP FUNCTION getPlayerData();
+DROP VIEW V_stats;
+DROP VIEW V_PlayerStatsLastGames;
+DROP VIEW V_PlayerStatsLatest;
+DROP VIEW V_PlayerStatsDaily;
+DROP VIEW v_gamesperperiod;
+DROP VIEW V_PlayersAllGames;
+DROP VIEW V_PlayerStatsAll;
 
-
-
-                  
+**/
