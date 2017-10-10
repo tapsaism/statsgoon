e416488cd7f4925616f73cdea5cab784e62c396a
Olli Iivonen <olliiivonen@Ollis-MacBook-Pro.local>
2017-10-10 21:53:59 +0300

    New player selector component

---
+++ statsgoon-front/src/Constants.js
@@ -0,0 +1,28 @@
+const constants = {
+
+  dataApiUrl : 'https://qgb6qyqmxb.execute-api.us-east-1.amazonaws.com/beta/statsgoon-data-api/v1/',
+
+  solverApiUrl : 'https://zegyactk64.execute-api.us-east-1.amazonaws.com/statsgoon/solver/solve',
+
+  positions : {
+    goalie : '1',
+    dmen : '2',
+    fwd : '3'
+  },
+
+  measures : [
+      { key: 'points_total', text: 'Total Points', value: 'points_total' },
+      { key: 'points_avg', text: 'Average Points', value: 'points_avg' },
+      { key: 'points_ga_only', text: 'Goals & Assists', value: 'points_ga_only' },
+      { key: 'points_wo_ga', text: 'Goals & Assists Excluded', value: 'points_wo_ga' },
+      { key: 'last_10_total', text: 'Last 10 games', value: 'last_10_total' },
+      { key: 'last_5_total', text: 'Last 5 games', value: 'last_5_total' },
+      { key: 'last_3_total', text: 'Last 3 games', value: 'last_3_total' },
+      { key: 'median', text: 'Median', value: 'median' }
+  ]
+
+}
+
+module.exports.getConstants = (type) => {
+  return constants[type]
+}
---
+++ statsgoon-front/src/components/StatsgoonPlayerSelector.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import Axios from 'axios'
+import { Button, Grid, Dropdown, Container } from 'semantic-ui-react'
+
+import Constants from '../Constants.js'
+import Utils from '../utils/StatsgoonUtils.js'
+
+class StatsgoonPlayerSelector extends React.Component {
+
+  constructor (props) {
+
+    super(props)
+
+    this.state = {
+      goalies: [],
+      dmen: [],
+      forwards: [],
+      selectedGoalies: [],
+      selectedDmen: [],
+      selectedForwards: [],
+      loading: true
+    }
+  }
+
+  componentDidMount () {
+    this.loadPlayers()
+  }
+
+  goalieChange = (e, { value }) => this.setState({selectedGoalies: value})
+  dmenChange = (e, { value }) => this.setState({selectedDmen: value})
+  forwardChange = (e, { value }) => this.setState({selectedForwards: value})
+
+  getDropdownElement = (player) => ({key: player.name, text: player.team +'-'+ player.name, value: player.name})
+
+  getPlayersByPosition = (data, position) => data.filter(player => player.position === position).map(player => this.getDropdownElement(player))
+
+  loadPlayers = () => {
+
+    Axios.get(Constants.getConstants('dataApiUrl')+'player/all-players')
+      .then((response) =>  {
+        this.setState({
+          goalies: this.getPlayersByPosition(response.data,'GOA').sort(Utils.dynamicSort('text')),
+          dmen: this.getPlayersByPosition(response.data,'DEF').sort(Utils.dynamicSort('text')),
+          forwards: this.getPlayersByPosition(response.data,'FWD').sort(Utils.dynamicSort('text')),
+          loading: false
+        })
+      })
+      .catch((error) => error);
+
+  }
+
+  getPlayerStats = () => {
+
+    let params = {
+      filter : [
+        this.state.selectedGoalies.concat(this.state.selectedDmen).concat(this.state.selectedForwards),
+        '2017-2018'
+        ]
+    }
+
+    this.props.drawCharts(params)
+
+  }
+
+  render () {
+    return(
+     <Container>
+     <Grid columns={4} stackable>
+       <Grid.Row>
+         <Grid.Column>
+          <Dropdown
+            search={true}
+            multiple={true}
+            loading={this.state.loading}
+            placeholder='Goalie'
+            onChange={this.goalieChange}
+            fluid selection options={this.state.goalies} />
+         </Grid.Column>
+         <Grid.Column>
+          <Dropdown
+            search={true}
+            multiple={true}
+            loading={this.state.loading}
+            placeholder='Defense'
+            onChange={this.dmenChange}
+            fluid selection options={this.state.dmen} />
+         </Grid.Column>
+         <Grid.Column>
+          <Dropdown
+            search={true}
+            multiple={true}
+            loading={this.state.loading}
+            placeholder='Forward'
+            onChange={this.forwardChange}
+            fluid selection options={this.state.forwards} />
+         </Grid.Column>
+         <Grid.Column>
+         <Button fluid basic color='blue' type='submit' onClick={this.getPlayerStats}>Get stats!</Button>
+         </Grid.Column>
+       </Grid.Row>
+      </Grid>
+     </Container>
+  )
+  }
+}
+
+export default StatsgoonPlayerSelector
