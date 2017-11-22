c9d540b01551344f9e6d3ad36dee43176cf15fe2
Olli Iivonen <olliiivonen@Ollis-MacBook-Pro.local>
2017-11-22 20:16:52 +0200

    Top player files

---
+++ statsgoon-front/src/components/StatsgoonTopPlayers.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Loader, Container, Header } from 'semantic-ui-react'
+import Axios from 'axios'
+import { Analytics } from 'aws-amplify'
+
+import Constants from '../Constants.js'
+import Actions from '../StatsgoonActions'
+import Table from './viz/StatsgoonTopPlayersTable'
+
+class StatsgoonTopPlayers extends React.Component {
+
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+      topAllPos: [],
+      topGoalies: [],
+      topDmen: [],
+      topFwd:[],
+      loaderStatus: 'disabled',
+      loaderContent: 'Loading top players'
+    }
+  }
+
+  componentDidMount () {
+    this.loadTopLists()
+  }
+
+  getFilter = (season,position,teams) => ({filter: [season,position,teams]})
+
+  loadTopLists = () => {
+
+    this.setState({loaderStatus: 'active'})
+
+    let paramsArray = [
+      this.getFilter("2017-2018",["GOA","DEF","FWD"],Constants.teams),
+      this.getFilter("2017-2018",["GOA"],Constants.teams),
+      this.getFilter("2017-2018",["DEF"],Constants.teams),
+      this.getFilter("2017-2018",["FWD"],Constants.teams)
+    ]
+
+    Actions.getTopPlayersAll(paramsArray)
+      .then(Axios.spread((all,goalie,dmen,fwd) =>  {
+        this.setState({topAllPos: all.data, topGoalies: goalie.data, topDmen: dmen.data, topFwd: fwd.data})
+        this.setState({loaderStatus: 'disabled'})
+      }))
+      .catch((error) => error)
+  }
+
+  createTables = () => {
+    return (
+      <Container>
+        <Header as='h5'>All positions</Header>
+        <Table chartData={this.state.topAllPos}/>
+        <Header as='h5'>Goalies</Header>
+        <Table chartData={this.state.topGoalies}/>
+        <Header as='h5'>Defensemen</Header>
+        <Table chartData={this.state.topDmen}/>
+        <Header as='h5'>Forwards</Header>
+        <Table chartData={this.state.topFwd}/>
+      </Container>
+    )
+  }
+
+  getFidgetSpinner = () => <Container><Loader className={this.state.loaderStatus} content={this.state.loaderContent}/></Container>
+
+  showContent = () => this.state.loaderStatus === 'disabled' ? this.createTables() : this.getFidgetSpinner()
+
+  render = () => {
+    Analytics.record('topPlayers')
+    return (
+      <Container>
+      {this.showContent()}
+      </Container>
+    )
+  }
+
+}
+
+
+export default StatsgoonTopPlayers
---
+++ statsgoon-front/src/components/viz/StatsgoonTopPlayersTable.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Table } from 'semantic-ui-react'
+import Utils from '../../utils/StatsgoonUtils.js'
+
+class StatsgoonTable extends React.Component {
+
+  createRows = (latestStats) => {
+
+    let latestStatsSorted = latestStats.sort(Utils.dynamicSort('HOCKEYGM_TOTAL'))
+
+    return latestStatsSorted.map(stat => {
+      return(
+      <Table.Row key={stat.name}>
+        <Table.Cell>{stat.season}</Table.Cell>
+        <Table.Cell>{stat.team}</Table.Cell>
+        <Table.Cell>{stat.name}</Table.Cell>
+        <Table.Cell>{stat.position}</Table.Cell>
+        <Table.Cell>{stat.hockeygm_total}</Table.Cell>
+        <Table.Cell>{stat.hockeygm_average}</Table.Cell>
+        <Table.Cell>{stat.hockeygm_value}</Table.Cell>
+        <Table.Cell>{stat.price_per_point}</Table.Cell>
+      </Table.Row>
+    )
+    })
+
+  }
+
+  render() {
+  return (
+    <Table fixed size='small' unstackable>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Season</Table.HeaderCell>
+          <Table.HeaderCell>Team</Table.HeaderCell>
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Position</Table.HeaderCell>
+          <Table.HeaderCell>Total</Table.HeaderCell>
+          <Table.HeaderCell>Avg</Table.HeaderCell>
+          <Table.HeaderCell>Value</Table.HeaderCell>
+          <Table.HeaderCell>Price per point</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+      {this.createRows(this.props.chartData)}
+      </Table.Body>
+    </Table>
+  )
+  }
+}
+
+export default StatsgoonTable
