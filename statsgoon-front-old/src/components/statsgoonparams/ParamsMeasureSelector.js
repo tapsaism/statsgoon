89bb8026de830e4e0632bf26eb402d56544179b2
Olli Iivonen <olliiivonen@Ollis-MacBook-Pro.local>
2018-02-13 11:22:57 +0200

    Removing a unused folder

--- statsgoon-front-old/src/components/statsgoonparams/ParamsMeasureSelector.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Dropdown } from 'semantic-ui-react'
-
-export default class StatsgoonParamsMeasureSelector extends React.Component {
-  render = () => <Dropdown placeholder='Measures' onChange={this.props.measureChange} fluid selection options={this.props.measures} />
-}
