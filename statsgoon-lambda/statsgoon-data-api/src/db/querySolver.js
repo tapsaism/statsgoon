080273060b1a7d241a3b2105fb171985c854b592
Olli Iivonen <olliiivonen@Ollis-MacBook-Pro.local>
2018-03-31 15:03:08 +0300

    API updated to match the new db structure

--- statsgoon-lambda/statsgoon-data-api/src/db/querySolver.js
+++ statsgoon-lambda/statsgoon-data-api/src/db/querySolver.js
@@ -1,109 +1,69 @@
 'use strict'
 
+const squel = require("squel");
+
 let queries = {
-  'player/all-stats':              `SELECT
-                                    *
-                                    FROM solver_data()
-                                    WHERE player IS NOT NULL
-                                    AND playername IN ($1:csv)
-                                    AND season = $2`,
+  'player/all-stats':             squel.select().from('api.solver_data')
+                                  .where('playername IN ($1:csv)')
+                                  .where('season = $2')
+                                  .toString(),
 
-  'player/all-players':            `SELECT
-                                    *
-                                    FROM D_PLAYER`,
+  'player/all-players':           squel.select().from('api.all_players').toString(),
 
-  'player/games-left':             `SELECT
-                                    DATE,
-                                    OPPONENT_ACRM,
-                                    PLAYER,
-                                    AWAYGAME,
-                                    HOMEGAME,
-                                    GAME
-                                    FROM v_teams_schedule_with_opponents_current_period games
-                                    INNER JOIN All_Players player
-                                    ON games.team = player.team
-                                    WHERE player IN ($1:csv)
-                                    ORDER BY DATE`,
+  'player/games-left':            squel.select().from('api.schedule_current_period_player')
+                                  .where('player IN ($1:csv)')
+                                  .order('date')
+                                  .toString(),
 
-  'player/daily-stats':            `SELECT
-                                  	*
-                                    FROM daily_stats()
-                                    WHERE player IN ($1:csv)
-                                    AND SEASON = $2
-                                    ORDER BY filedate`,
+  'player/daily-stats':           squel.select().from('api.daily_stats')
+                                  .where('player IN ($1:csv)')
+                                  .where('season = $2')
+                                  .order('filedate')
+                                  .toString(),
 
-  'player/top-players':            `SELECT
-                                    SEASON,
-                                    TEAM,
-                                    NAME,
-                                    POSITION,
-                                    HOCKEYGM_TOTAL,
-                                    HOCKEYGM_AVERAGE,
-                                    HOCKEYGM_VALUE,
-                                    PRICE_PER_POINT
-                                    FROM F_TOP_PLAYERS_BY_SEASON
+  'player/top-players':           squel.select().from('api.top_players_by_season')
+                                  .where('season = $1')
+                                  .where('position IN ($2:csv)')
+                                  .where('team IN ($3:csv)')
+                                  .order('hockeygm_total', false)
+                                  .limit(10)
+                                  .toString(),
 
-                                    WHERE SEASON = $1
-                                    AND POSITION IN ($2:csv)
-                                    AND TEAM IN ($3:csv)
+  'team/games-left':              squel.select().from('api.team_games_left').toString(),
 
-                                    ORDER BY hockeygm_total DESC
+  'team/schedule-current-period': squel.select().from('api.schedule_current_period')
+                                  .field('*')
+                                  .field('SUM(game) OVER (PARTITION BY team) as games_total')
+                                  .field('SUM(homegame) OVER (PARTITION BY team) as home_games_total')
+                                  .field('SUM(awaygame) OVER (PARTITION BY team) as away_games_total')
+                                  .order('games_total', false)
+                                  .order('team')
+                                  .order('date')
+                                  .toString(),
 
-                                    LIMIT 10`,
+  'team/schedule-with-params':    squel.select().from('api.schedule')
+                                  .field('*')
+                                  .field('SUM(game) OVER (PARTITION BY team) as games_total')
+                                  .field('SUM(homegame) OVER (PARTITION BY team) as home_games_total')
+                                  .field('SUM(awaygame) OVER (PARTITION BY team) as away_games_total')
+                                  .where('date BETWEEN $1 and $2')
+                                  .order('games_total', false)
+                                  .order('team')
+                                  .order('date')
+                                  .toString(),
 
-  'team/games-left':               `SELECT
-                                    team,
-                                    SUM(game) games
-                                    FROM V_PeriodGamesLeft
-                                    GROUP BY team
-                                    ORDER BY games DESC`,
-
-  'team/schedule-current-period':  `SELECT
-                                    DATE,
-                                    SEASON,
-                                    HOCKEYGMPERIOD,
-                                    TEAM,
-                                    TEAM_ACRM,
-                                    AWAYGAME,
-                                    HOMEGAME,
-                                    GAME,
-                                    OPPONENT,
-                                    OPPONENT_ACRM,
-                                    SUM(GAME) OVER (PARTITION BY TEAM) AS GAMES_TOTAL
-                                    FROM v_teams_schedule_with_opponents_current_period
-                                    ORDER BY games_total DESC, team, date`,
-
-  'team/schedule-with-params':     `SELECT
-                                    DATE,
-                                    SEASON,
-                                    HOCKEYGMPERIOD,
-                                    TEAM,
-                                    TEAM_ACRM,
-                                    AWAYGAME,
-                                    HOMEGAME,
-                                    GAME,
-                                    OPPONENT,
-                                    OPPONENT_ACRM,
-                                    SUM(game) OVER (PARTITION BY team) as games_total
-                                    FROM v_teams_schedule_with_opponents
-                                    WHERE date between $1 and $2
-                                    ORDER BY games_total DESC, team, date`,
-
-    'team/points':                  `SELECT
-                                     SEASON,
-                                     TEAM,
-                                     SUM(hockeygm_total)::integer HOCKEYGM_TOTAL,
-                                     CAST(AVG(CAST(hockeygm_average AS DECIMAL)) AS INT) HOCKEYGM_AVERAGE,
-                                     CAST(AVG(CAST(hockeygm_value AS INT)) AS INT) HOCKEYGM_VALUE
-
-                                     FROM F_TOP_PLAYERS_BY_SEASON
-
-                                     WHERE SEASON = $1
-                                     AND POSITION IN ($2:csv)
-
-                                     GROUP BY SEASON,TEAM
-
-                                     ORDER BY 3 DESC`
+  'team/points':                  squel.select().from('api.team_points_by_season')
+                                  .field('season')
+                                  .field('team')
+                                  .field('SUM(hockeygm_total) AS hockeygm_total')
+                                  .field('ROUND(AVG(hockeygm_average)::numeric, 2) AS hockeygm_average')
+                                  .field('AVG(hockeygm_value)::int AS hockeygm_value')
+                                  .group('season')
+                                  .group('team')
+                                  .where('season = $1')
+                                  .where('position IN ($2:csv)')
+                                  .order('hockeygm_total', false)
+                                  .toString()
 }
 
 module.exports.getQueryByRequestType = (requestType) => {
