66770609309417952aa42cba90c6689197ab608e
Olli Iivonen <olliiivonen@Ollis-MacBook-Pro.local>
2018-04-02 08:51:37 +0300

    Refactored the summary view and added line and pp information

--- statsgoon-front/src/components/charts/StatsgoonSummary.js
+++ statsgoon-front/src/components/charts/StatsgoonSummary.js
@@ -8,69 +8,103 @@ export default class StatsgoonSummary extends React.Component {
 
     if (players.length >= index+1) {
       return(
-        <Statistic size='mini'
-          value={players[index].playername}
-          label={players[index].team_acronym+ ' - ' + players[index].position + ' - ' + players[index].value + ' - ' + players[index].points_avg}
-        />
+        <Statistic size='mini'>
+          <Statistic.Value>{players[index].playername}</Statistic.Value>
+          <Statistic.Label>{players[index].team_acronym + ' - ' +
+                  players[index].line + ' line - '}</Statistic.Label>
+        </Statistic>
       )
     }
   }
 
-  getSummary = (stats,games) => {
+  addRows = (data,pos) => {
+    return data.filter(stat => stat.position === pos).map(rowData => this.addRow(rowData))
+  }
+
+  addRow = (data) => {
+    console.log(data)
+    return (
+      <Table.Row>
+        <Table.Cell><Statistic size='mini' value={data.team_acronym} /></Table.Cell>
+        <Table.Cell><Statistic size='mini' value={data.playername} /></Table.Cell>
+        <Table.Cell><Statistic color={data.line === null ? 'red' : ''} size='mini' value={data.line === null ? 'N/A' : data.line + '.'} /></Table.Cell>
+        <Table.Cell><Statistic size='mini' value={data.plays_pp === 1 ? 'Yes' : 'No'} /></Table.Cell>
+        <Table.Cell><Statistic size='mini' value={data.plays_pp === 1 ? data.pp_unit+'.' : 'N/A'} /></Table.Cell>
+        <Table.Cell><Statistic size='mini' value={data.value} /></Table.Cell>
+        <Table.Cell><Statistic size='mini' value={data.points_avg} /></Table.Cell>
+        <Table.Cell><Statistic size='mini' value={data.points_total} /></Table.Cell>
+      </Table.Row>
+    )
+  }
+
+  addHeaderRow = () => {
+    return (
+      <Table.Row>
+        <Table.Cell><Statistic size='mini' label='team'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='name'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='line'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='PP'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='PP unit'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='value'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='avg'/></Table.Cell>
+        <Table.Cell><Statistic size='mini' label='total'/></Table.Cell>
+      </Table.Row>
+    )
+  }
+
+  getTotals = (stats,games) => {
     return (
       <Container>
       <Container textAlign='center'>
-        <h3>Stats</h3>
+        <h3>Results</h3>
       </Container>
+      <Divider hidden />
       <Container textAlign='center'>
-        <Statistic size='small'
-          value={Math.round(stats.reduce((sum,data) => (sum + data.points_avg),0))}
-          label='Daily points'
-        />
-        <Statistic size='small'
+      <Statistic.Group widths='six' size='tiny'>
+        <Statistic
           value={stats.reduce((sum,data) => (sum + parseInt(data.value,10)),0)}
           label='Team Value'
         />
-        <Statistic size='small'
+        <Statistic
           value={Math.round(games.reduce((sum,data) => (sum + data.game),0))}
           label='Games left'
         />
+        <Statistic
+          value={Math.round(stats.reduce((sum,data) => (sum + data.points_avg),0))}
+          label='Daily points'
+        />
+        <Statistic
+          value={stats.reduce((sum,data) => (sum + data.plays_pp),0) + ' / 5'}
+          label='Powerplay'
+        />
+        <Statistic
+          value={stats.reduce((sum,data) => (sum + (data.pp_unit === '1' ? 1 : 0)),0) + ' / 5'}
+          label='PP 1. Unit'
+        />
+        <Statistic
+          value={stats.reduce((sum,data) => (sum + (data.pp_unit === '2' ? 1 : 0)),0) + ' / 5'}
+          label='PP 2. Unit'
+        />
+      </Statistic.Group>
       </Container>
-      </Container>
+    </Container>
     )
   }
 
   getTeam = (stats) => {
     return(
       <Container>
-        <Container textAlign='center'>
-          <h3>Team</h3>
-        </Container>
         <Divider />
         <Container>
         <Table basic='very'>
         <Table.Body>
-          <Table.Row>
-            <Table.Cell textAlign='center'>{this.getPlayerDetails(stats,0,'FWD')}</Table.Cell>
-            <Table.Cell></Table.Cell>
-            <Table.Cell textAlign='center'>{this.getPlayerDetails(stats,1,'FWD')}</Table.Cell>
-            <Table.Cell></Table.Cell>
-            <Table.Cell textAlign='center'>{this.getPlayerDetails(stats,2,'FWD')}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell></Table.Cell>
-            <Table.Cell textAlign='center'>{this.getPlayerDetails(stats,0,'DEF')}</Table.Cell>
-            <Table.Cell></Table.Cell>
-            <Table.Cell textAlign='center'>{this.getPlayerDetails(stats,1,'DEF')}</Table.Cell>
-            <Table.Cell></Table.Cell>
-          </Table.Row>
-          <Table.Row textAlign='center'>
-            <Table.Cell></Table.Cell>
-            <Table.Cell></Table.Cell>
-            <Table.Cell>{this.getPlayerDetails(stats,0,'GOA')}</Table.Cell>
-            <Table.Cell></Table.Cell>
-            <Table.Cell></Table.Cell>
-          </Table.Row>
+          {this.addHeaderRow()}
+          <Table.Row><Table.Cell>Forwards</Table.Cell></Table.Row>
+          {this.addRows(stats,'FWD')}
+          <Table.Row><Table.Cell>Defencemen</Table.Cell></Table.Row>
+          {this.addRows(stats,'DEF')}
+          <Table.Row><Table.Cell>Goalies</Table.Cell></Table.Row>
+          {this.addRows(stats,'GOA')}
         </Table.Body>
         </Table>
         </Container>
@@ -81,9 +115,8 @@ export default class StatsgoonSummary extends React.Component {
   getStatistics = (stats,games) => {
     return (
           <Container>
+            {this.getTotals(stats,games)}
             {this.getTeam(stats)}
-            <Divider />
-            {this.getSummary(stats,games)}
           </Container>
     )
   }
